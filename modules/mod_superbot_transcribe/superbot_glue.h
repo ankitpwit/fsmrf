999a9798f3f6c4263f5264ec962e7bb1ac8fd7d2
ankitpwit <ankit@pinnacleworks.net>
2024-07-30 17:03:32 +0530

    Update superbot_glue.h

--- modules/mod_superbot_transcribe/superbot_glue.h
+++ modules/mod_superbot_transcribe/superbot_glue.h
@@ -4,7 +4,7 @@
 switch_status_t superbot_speech_init();
 switch_status_t superbot_speech_cleanup();
 switch_status_t superbot_speech_session_init(switch_core_session_t *session, responseHandler_t responseHandler, 
-		uint32_t samples_per_second, uint32_t channels, char* lang, char* dur_type, char* request_id, char *bugname, void **ppUserData);
+		uint32_t samples_per_second, uint32_t channels, char* lang, char* dur_type, char* request_id, char* silence_duration, char *bugname, void **ppUserData);
 switch_status_t superbot_speech_session_cleanup(switch_core_session_t *session, int channelIsClosing, switch_media_bug_t *bug);
 switch_bool_t superbot_speech_frame(switch_media_bug_t *bug, void* user_data);
 
